3da5144d1ca147cbdf10207249e155f2ceeaf681
Carlos Garcia Sanchez-Cano <carlosga@c1r6s3.42madrid.com>
2024-06-11 18:33:14 +0200

    luz cilinidros paralelos a eje funciona, torcidos no

--- src/lights.c
+++ src/lights.c
@@ -51,24 +51,28 @@ double	get_brightness_level_cylinder(t_cylinder *cy, t_light *l, t_vec p)
 		return (alpha);
 	}
 	//Calcula el punto del eje a la altura del punto de impacto del rayo en el cilindro
-	t_vec h = vec_sub(p, cy->o);
+	t_vec h = vec_sub(cy->o, p);
     double t_proj = dot_prod(h, cy->v);
     t_vec axis_point = vec_add(cy->o, producto_escalar(cy->v, t_proj));
 
 	//Calcula el rayo perpendicular al eje del cilindro que cruza con el origen del rayo
-    t_vec oc = vec_sub(l->o, cy->o);
-    double projection_length = dot_prod(oc, cy->v);
-    t_vec q = vec_add(cy->o, producto_escalar(cy->v, projection_length));
+    t_vec oc = vec_sub(cy->o, l->o);
+    double projection_length = dot_prod(oc, normalize(cy->v));
+    t_vec q = vec_add(cy->o, producto_escalar(normalize(cy->v), projection_length));
 
     // Vector perpendicular
     v2 = normalize(vec_sub(l->o, q));
 	v1 = normalize(*create_vector(axis_point, p));
 	
-	printf("v1(%f, %f, %f) --- v2(%f, %f, %f)\n", v1.x, v1.y, v1.z, v2.x, v2.y, v2.z);
-	alpha = sqrt(dot_prod(v1, v2) / (module(v1) * module(v2)));
+	//printf("v1(%f, %f, %f) --- v2(%f, %f, %f) --- %f\n", v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, dot_prod(v1, v2));
+	double d = dot_prod(v1, neg(v2)) / (module(v1) * module(v2));
+	if (d < 0)
+		return 0;
+	alpha = sqrt(d);
+	printf("%f, %f\n", dot_prod(v1, v2) / (module(v1)* module(v2)), alpha);
 	if (alpha < 0)
 		return (0);
-	if (alpha > 255)
+	if (alpha > 1)
 		printf("alpha: %f", alpha);
 
 	return (alpha);
